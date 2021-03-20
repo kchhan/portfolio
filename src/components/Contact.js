92c8203b289202f6f2da0123a1c5608a25511ad5
Kevin Chhan <kvnchhan@gmail.com>
2021-03-20 10:21:24 -0700

    add mars-photos and update contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,30 +1,33 @@
-import Header from './Header';
-import Footer from './Footer';
-import { Spring } from 'react-spring/renderprops';
+import Header from "./Header";
+import Footer from "./Footer";
+import {Spring} from "react-spring/renderprops";
 
 export default function Contact() {
-	return (
-		<section className='h-screen flex flex-col items-center justify-between bg-dark_mode-default text-gray-500'>
-			<Header />
+    return (
+        <section className="h-screen flex flex-col items-center justify-between bg-dark_mode-default text-gray-500">
+            <Header />
 
-			<Spring
-				delay={1000}
-				from={{ opacity: 0, transform: 'translate3d(0, -100px, 0)' }}
-				to={{ opacity: 1, transform: 'translate3d(0, 0px, 0)' }}
-			>
-				{({opacity, transform}) => (
-					<div style={{ opacity, transform }}>
-						<h1 className='text-xl'>I'm Available! Let's Chat!</h1>
-						<h2 className='p-2 text-xl border text-center text-white'>
-							<a href='mailto:kvnchhan@gmail.com' className='p-2'>
-								kvnchhan@gmail.com
-							</a>
-						</h2>
-					</div>
-				)}
-			</Spring>
-			
-			<Footer />
-		</section>
-	);
+            <Spring
+                delay={1000}
+                from={{opacity: 0, transform: "translate3d(0, -100px, 0)"}}
+                to={{opacity: 1, transform: "translate3d(0, 0px, 0)"}}
+            >
+                {({opacity, transform}) => (
+                    <div style={{opacity, transform}}>
+                        <h1 className="text-xl">I'm Available! Let's Chat!</h1>
+                        <h2 className="p-2 text-xl border text-center text-white">
+                            <a
+                                href="mailto:kevinchhan1@gmail.com"
+                                className="p-2"
+                            >
+                                kevinchhan1@gmail.com
+                            </a>
+                        </h2>
+                    </div>
+                )}
+            </Spring>
+
+            <Footer />
+        </section>
+    );
 }
